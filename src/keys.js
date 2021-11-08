e8803cd8a945cf9df63fc7c1c09fc62b4cf193a5
zCodeLAT <esteban.martin7@gmail.com>
2021-11-08 19:01:39 -0300

    add .env data

--- src/keys.js
+++ src/keys.js
@@ -2,7 +2,7 @@ module.exports = {
   database:{
     connectionLimit : 5, //for testing
     //socketPath : '/Applications/MAMP/Library/bin/mysql',
-    port: '3306',
+    port: process.env.DB_PORT || '3306',
     host: process.env.DB_HOST || 'localhost', // variables de entorno
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '1234',
